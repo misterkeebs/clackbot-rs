397489806b5a6bd370a09fdc945d44cf556546da
Felipe Coury <felipe.coury@gmail.com>
2024-04-16 20:58:09 -0300

    feat: live wpm

--- firefox/inject.js
+++ firefox/inject.js
@@ -1,25 +1,43 @@
-(function() {
+(function () {
   let wpmVisible = false;
+  let currentWpm = 0;
+
+  function checkLiveWpm() {
+    const wpmEl = document.querySelectorAll("#miniTimerAndLiveWpm > .wpm");
+    if (wpmEl && wpmEl[0]) {
+      const wpm = wpmEl[0].textContent;
+      if (wpm != currentWpm) {
+        fetch(
+          `https://clackbot-rs-production.up.railway.app/liveWpm?wpm=${wpm}`,
+        );
+      }
+    }
+    setTimeout(checkLiveWpm, 200);
+  }
+
   function checkForWpm() {
     const wpmEl = document.querySelectorAll('.wpm > [data-balloon-pos="up"]');
     if (wpmVisible) {
       if (!wpmEl || !wpmEl[0] || wpmEl[0].offsetParent === null) {
-        console.log('WPM went away');
+        console.log("WPM went away");
         wpmVisible = false;
       }
     } else {
       if (wpmEl && wpmEl[0] && wpmEl[0].offsetParent) {
         const wpm = wpmEl[0].textContent;
-        if (wpm.indexOf('-') === -1) {
-          console.log('wpm', wpm);
+        if (wpm.indexOf("-") === -1) {
+          console.log("wpm", wpm);
           wpmVisible = true;
-          fetch(`https://clackbot-rs-production.up.railway.app/wpm?wpm=${wpm}`);
+          fetch(
+            `https://clackbot-rs-production.up.railway.app/finishWpm?wpm=${wpm}`,
+          );
         }
       }
     }
     setTimeout(checkForWpm, 200);
   }
 
-  console.log('checking for wpm...');
+  console.log("checking for wpm...");
   checkForWpm();
+  checkLiveWpm();
 })();
--- src/http_server.rs
+++ src/http_server.rs
@@ -1,14 +1,33 @@
-use crate::{twitch::TWITCH, WPM_GAME};
+use std::sync::atomic::Ordering;
+
+use crate::{twitch::TWITCH, LIVE_WPM, WPM_GAME};
 
 pub async fn start(port: String) -> anyhow::Result<()> {
     let mut app = tide::new();
-    app.at("/").get(|_| async { Ok("So empty...") });
-    app.at("/wpm").get(register_wpm);
+    app.at("/").serve_dir("public/")?;
+    app.at("/liveWpm").get(|_| async move {
+        let wpm = LIVE_WPM.get().unwrap().load(Ordering::Relaxed).to_string();
+        Ok(wpm)
+    });
+    app.at("/finishWpm").get(register_wpm);
+    app.at("/setLiveWpm").get(set_live_wpm);
     let address = format!("0.0.0.0:{port}");
     println!("Listening on: {}", address);
     app.listen(address).await?;
 
     Ok(())
+}
+
+async fn set_live_wpm(req: tide::Request<()>) -> tide::Result {
+    let Some(query) = req.url().query_pairs().next() else {
+        return Ok(tide::Response::new(400));
+    };
+    let wpm = query.1;
+    let wpm = wpm.parse::<u8>()?;
+
+    LIVE_WPM.get().unwrap().store(wpm, Ordering::Relaxed);
+
+    Ok(tide::Response::new(200))
 }
 
 async fn register_wpm(req: tide::Request<()>) -> tide::Result {
